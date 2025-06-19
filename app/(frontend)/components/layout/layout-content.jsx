7eed69d764c2edd4d5a20dfa8acb0d90bb2c92e6
Kittisak <kid00345@hotmail.com>
2025-06-19 17:57:08 +0700

    update

--- app/(frontend)/components/layout/layout-content.jsx
+++ app/(frontend)/components/layout/layout-content.jsx
@@ -1,17 +1,6 @@
 'use client';
 
 import {
-  Accordion,
-  AccordionContent,
-  AccordionItem,
-  AccordionTrigger,
-} from '@/components/ui/accordion';
-import { BlockList } from '../../account/security/privacy-settings/components/block-list';
-import { ReportSettings } from '../../account/security/privacy-settings/components/report-settings';
-import { EntryCallout, Teams } from '../demo1/light-sidebar';
-import { Integrations, ManageData, MyBalance, Options } from './components';
-
-import {
   SwiperCarousel,
   CardNews,
 } from './components';
